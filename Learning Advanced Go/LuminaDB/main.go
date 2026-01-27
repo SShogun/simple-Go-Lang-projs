169ea8beeee141a1debb3db7de9f55c41f67b060
Soham Shirke <sohamcodes101@gmail.com>
2026-01-27 23:00:45 +0530

    Add in-memory store and main engine for LuminaDB
    Introduces a thread-safe in-memory key-value store (RWData) and the LuminaDB engine struct, integrating logging and basic CRUD operations. Adds a main function to demonstrate usage of Put, Get, and Delete methods.

--- Learning Advanced Go/LuminaDB/main.go
+++ Learning Advanced Go/LuminaDB/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"sync"
 )
@@ -10,6 +11,70 @@ type Logger struct {
 	mu   sync.Mutex
 }
 
+// Memory Store
+type RWData struct {
+	mu    sync.RWMutex
+	value map[string]string
+}
+
+func (d *RWData) Get(key string) string {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	return d.value[key]
+}
+
+func (d *RWData) Set(key, value string) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.value[key] = value
+}
+
+func (d *RWData) Delete(key string) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	delete(d.value, key)
+}
+
+// Main Engine
+type LuminaDB struct {
+	store  *RWData
+	logger *Logger
+}
+
+func NewLuminaDB(logFile string) (*LuminaDB, error) {
+	l, err := NewLogger(logFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LuminaDB{store: &RWData{value: make(map[string]string)}, logger: l}, nil
+}
+
+func (db *LuminaDB) Put(key, value string) error {
+	if err := db.logger.LogSet(key, value); err != nil {
+		return fmt.Errorf("Failed to log to disk: %w", err)
+	}
+	db.store.Set(key, value)
+	return nil
+}
+
+func (db *LuminaDB) Get(key string) string {
+	return db.store.Get(key)
+}
+
+// Delete removes from Disk then Memory
+func (db *LuminaDB) Delete(key string) error {
+	if err := db.logger.LogDelete(key); err != nil {
+		return fmt.Errorf("failed to log delete: %w", err)
+	}
+
+	db.store.Delete(key)
+	return nil
+}
+
+func (db *LuminaDB) Close() error {
+	return db.logger.Close()
+}
 func NewLogger(filename string) (*Logger, error) {
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -37,4 +102,18 @@ func (l *Logger) LogDelete(key string) error {
 
 func (l *Logger) Close() error {
 	return l.file.Close()
+}
+
+func main() {
+	db, err := NewLuminaDB("lumina.llog")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.Put("session_1", "active")
+	db.Put("session_2", "idle")
+	fmt.Println("Session 1 Status:", db.Get("session_1"))
+
+	db.Delete("session_2")
 }
